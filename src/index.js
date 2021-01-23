2140d8dc66d11ff942c6bbf0d2f27597ed611a51
bakpawel <bakpawel103@gmail.com>
2021-01-23 13:24:42 +0100

    Changed to heroku

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
 const express = require('express');
 const Routes = require('./Routes');
 const app = express();
-const port = process.env.PORT || 4000;
+const port = 4000;
 
 let routes = null;
 
 app.get('/', (req, res) => {
-    return res.json(routes.getChildEndpoints(req.route.path));
+    return res.type('json').send(JSON.stringify(routes.getChildEndpoints(req.route.path), null, 2));
 });
 
 app.get('/projects', (req, res) => {
-    return res.json(routes.getChildEndpoints(req.route.path));
+    return res.type('json').send(JSON.stringify(routes.getChildEndpoints(req.route.path), null, 2));
 });
 
 app.get('/projects/:name', (req, res) => {
-    return res.json(routes.getChildEndpoints(req.route.path));
+    return res.type('json').send(JSON.stringify(routes.getChildEndpoints(req.route.path), null, 2));
 });
 
 app.listen(port, () => {
